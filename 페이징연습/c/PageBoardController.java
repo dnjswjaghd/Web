350afd23515484c594e105e7f3da31c5bdda1243
dnjswjaghd <74960228+dnjswjaghd@users.noreply.github.com>
2022-02-09 09:13:56 +0900

    Update PageBoardController.java

--- 페이징연습/c/PageBoardController.java
+++ 페이징연습/c/PageBoardController.java
@@ -1,6 +1,11 @@
 package page.board.control;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
@@ -10,11 +15,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.oreilly.servlet.MultipartRequest;
+import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
+import com.oreilly.servlet.multipart.FileRenamePolicy;
+
 import page.board.domain.PageBoard;
+import page.board.model.FileSet;
 import page.board.model.PageBoardService;
-
-
-
 
 @WebServlet("/page/board.do")
 public class PageBoardController extends HttpServlet {
@@ -25,14 +32,18 @@ public class PageBoardController extends HttpServlet {
 			m = m.trim();
 			switch (m) {
 				case "list": list(request, response); break;
+				case "content": content(request, response); break;
+				case "input": input(request, response); break;
+				case "upload": upload(request, response); break;
+				case "download": download(request, response); break;
 			}
 		}else {
 			list(request, response);
 		}
 	}
 	private void list(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//service¿« ø™«“
-		//1. Model «⁄µÈ∏µ(java)
+		//serviceÏùò Ïó≠Ìï†
+		//1. Model Ìï∏Îì§ÎßÅ(java)
 		PageBoardService service = PageBoardService.getInstance();
 		int ps=0;
 		if(request.getParameter("ps")!=null) {
@@ -42,18 +53,26 @@ public class PageBoardController extends HttpServlet {
 			ps = 3;
 		}
 		ArrayList<PageBoard> list = service.listS(ps);
-		int page = 1; // Ω√¿€ ∆‰¿Ã¡ˆ
+		int page = 1;
+		String pageStr=request.getParameter("page");
+		if(pageStr != null) {
+			page = Integer.parseInt(pageStr);
+		}
 		int totalCount = list.size();
 		int totalPage = (int) Math.ceil(totalCount / (double) ps);
+		System.out.println("totalpage: "+totalPage);
 		int pageCount = 5;
 		int startPage = ((page - 1) / pageCount) * pageCount + 1;
 		int endPage = startPage + pageCount - 1;
-		// √ﬂ∞°∑Œ ¡∂∞« º≥¡§
+		// Ï∂îÍ∞ÄÎ°ú Ï°∞Í±¥ ÏÑ§Ï†ï
 		if( totalPage < page) {
 			page = totalPage;
 		}
-		if ( endPage > totalPage) {
+		if ( endPage > totalPage) { 
 			endPage = totalPage;
+		}
+		if (ps>totalCount) {
+			ps=totalCount;
 		}
 		request.setAttribute("list", list);
 		request.setAttribute("totalCount", totalCount);
@@ -62,10 +81,95 @@ public class PageBoardController extends HttpServlet {
 		request.setAttribute("startPage", startPage);
 		request.setAttribute("endPage", endPage);
 		request.setAttribute("page", page);
+		request.setAttribute("ps", ps);
 		
-		//2. View ¡ˆ¡§(jsp)
+		//2. View ÏßÄÏ†ï(jsp)
 		String view = "list.jsp";
 		RequestDispatcher rd = request.getRequestDispatcher(view);
 		rd.forward(request, response);
+	}
+	private void content(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PageBoardService service = PageBoardService.getInstance();
+		
+		
+		response.sendRedirect("content.jsp");
+	}
+	private void input(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PageBoardService service = PageBoardService.getInstance();
+		
+		
+		String view = "input.jsp";
+		response.sendRedirect(view);
+	}
+	private void upload(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PageBoardService service = PageBoardService.getInstance();
+		String saveDir = FileSet.FILE_DIR;
+		File fSaveDir =new File(saveDir);
+		if(!fSaveDir.exists()) fSaveDir.mkdirs();
+		
+		MultipartRequest mr = null;
+		FileRenamePolicy policy = new DefaultFileRenamePolicy();
+		int maxPostSize = 1*1024*1024;
+		try {
+			mr = new MultipartRequest(request, saveDir, maxPostSize,"utf-8", policy);
+		}catch(IOException ie) {
+			System.out.println("uploadÏã§Ìå® FileSet/111 line");
+		}
+		
+		String fname = mr.getFilesystemName("fname");
+		String ofname = mr.getOriginalFileName("fname");
+		
+		String view = "board.do";
+		response.sendRedirect(view);
+	}
+	private void download(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PageBoardService service = PageBoardService.getInstance();
+		String saveDir = FileSet.FILE_DIR;
+		String fname = request.getParameter("fname");
+		File f = new File(saveDir, fname);
+		
+		response.setContentType("application/octet-stream"); 
+		String Agent=request.getHeader("USER-AGENT");
+		if( Agent.indexOf("MSIE") >= 0 ){
+			int i = Agent.indexOf( 'M', 2 );
+			String IEV = Agent.substring( i + 5, i + 8 );
+			if( IEV.equalsIgnoreCase("5.5") ){
+				response.setHeader("Content-Disposition", "filename=" + new String( fname.getBytes("utf-8"), "8859_1") );
+			}else{
+				response.setHeader("Content-Disposition", "attachment;filename="+new String(fname.getBytes("utf-8"),"8859_1"));
+			}
+		}else{
+			response.setHeader("Content-Disposition", "attachment;filename=" + new String( fname.getBytes("utf-8"), "8859_1") );
+		}
+		
+		if(f.exists() && f.isFile()) {
+			FileInputStream fis = null;
+			OutputStream os = null;
+			BufferedInputStream bis = null;
+			BufferedOutputStream bos = null;
+			try {
+				fis = new FileInputStream(f); // Data Source(ÏÑúÎ≤ÑÏ∏° ÌååÏùº)
+				bis = new BufferedInputStream(fis, 2048);
+				os = response.getOutputStream();// Data Destination(ÌÅ¥ÎùºÏù¥Ïñ∏Ìä∏ Î∏åÎùºÏö∞Ï†Ä) ÌÅ¥ÎùºÏù¥Ïñ∏Ìä∏Í∞ùÏ≤¥Ïóê Ïì∞Í≥†Ïã∂Îã§-> response 
+				bos = new BufferedOutputStream(os, 2048);
+				
+				int count = 0;
+				byte b[] = new byte[1024];
+				while((count = bis.read(b)) != -1) {
+					bos.write(b, 0, count);
+				}
+				bos.flush();
+			}catch(IOException ie) {
+				
+			}finally {
+				if(bis != null) bis.close();
+				if(bos != null) bos.close();
+				if(os!=null) os.close();
+				if(fis!=null) fis.close();
+			}
+		}
+		
+		String view = "input.jsp";
+		response.sendRedirect(view);
 	}
 }

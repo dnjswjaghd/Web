01c785ed0702312b86e2f5cc02298a178d21eb0b
dnjswjaghd <74960228+dnjswjaghd@users.noreply.github.com>
2022-02-17 09:31:20 +0900

    Delete constant directory

--- constant/CategoryConst.java
+++
@@ -1,8 +0,0 @@
-package team1.togather.constant;
-
-public class CategoryConst {
-	public static final String CATEGORY_IT = "IT";
-	public static final String CATEGORY_SPORTS = "½ºÆ÷Ã÷";
-	public static final String CATEGORY_AMITY = "»ç±³";
-	
-}

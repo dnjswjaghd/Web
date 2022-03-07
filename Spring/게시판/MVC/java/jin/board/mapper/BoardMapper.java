c887e8796a6c7c75b30075b8f77c99d2595ce717
dnjswjaghd <74960228+dnjswjaghd@users.noreply.github.com>
2022-03-07 14:04:56 +0900

    Update BoardMapper.java

--- Spring/게시판/MVC/java/jin/board/mapper/BoardMapper.java
+++ Spring/게시판/MVC/java/jin/board/mapper/BoardMapper.java
@@ -3,7 +3,7 @@ package jin.board.mapper;
 import java.util.List;
 
 import jin.board.domain.Board;
-import jin.board.domain.BoardVo;
+import jin.board.domain.BoardVo; 
 
 public interface BoardMapper {
 	List<Board> selectPerPage(BoardVo boardvo);

9c8e034cd28ada491c7435bcbae5c1048589f074
dnjswjaghd <74960228+dnjswjaghd@users.noreply.github.com>
2022-03-10 09:23:21 +0900

    Update AjaxT01Controller.java

--- Spring/게시판/Restful/java/jin/md/controller/AjaxT01Controller.java
+++ Spring/게시판/Restful/java/jin/md/controller/AjaxT01Controller.java
@@ -5,7 +5,7 @@ import java.io.PrintWriter;
 import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
-
+ 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;

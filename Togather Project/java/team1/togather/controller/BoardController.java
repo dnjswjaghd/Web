2a980c191764abb2e5c20048c7f773821f78e9a8
dnjswjaghd <74960228+dnjswjaghd@users.noreply.github.com>
2022-03-18 10:20:10 +0900

    Update BoardController.java

--- Togather Project/java/team1/togather/controller/BoardController.java
+++ Togather Project/java/team1/togather/controller/BoardController.java
@@ -1,5 +1,10 @@
 package team1.togather.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,18 +13,48 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import team1.togather.domain.Board;
 import team1.togather.domain.BoardCriteria;
 import team1.togather.domain.PageMaker;
+import team1.togather.domain.Reply;
 import team1.togather.service.BoardServiceImpl;
+import team1.togather.service.ReplyServiceImpl;
 
 @Controller
 @RequestMapping("/board")
 public class BoardController {
 	@Autowired
 	private BoardServiceImpl service;
+	@Autowired
+	private ReplyServiceImpl replyservice;
 	
+	@ResponseBody
+	@GetMapping("listRest")
+	public List<Board> listRest(String option, String boardSearch, Integer page, Integer pageSize){
+		System.out.println("listRest안 option: "+option+" boardSearch: "+boardSearch+" page: "+page+" pagesize: "+pageSize);
+		
+		Map<String, String> map = new HashMap<String, String>();
+		BoardCriteria cri = new BoardCriteria(page, pageSize);
+		map.put("option", option);
+		map.put("boardSearch", boardSearch);
+		map.put("startRow", String.valueOf(cri.getStartRow()));
+		map.put("endRow", String.valueOf(cri.getEndRow()));
+		/*
+		if(option.equals("btitle")){
+			
+		}
+		if(option.equals("bcategory")){
+			
+		}
+		if(option.equals("mname")){
+			
+		}
+		*/
+		return service.getBoardBySearch(map);
+	}
 	@GetMapping("/listCri")
 	public void listCriGET(Model model, BoardCriteria cri) {
 		model.addAttribute("boardList", service.listCri(cri));
@@ -47,7 +82,33 @@ public class BoardController {
 		model.addAttribute("cri", cri);
 		return "board/boardMain";
 	}
-	
+	@PostMapping("boardWithSearch")
+	public String boardListWithSearch(BoardCriteria cri, Model model, HttpServletRequest request) {
+		System.out.println("board컨트롤러안 listpageGET: cri값: " +cri);
+		model.addAttribute("boardList", service.listCri(cri)); // =listPageCri()
+		if(request.getParameter("page")!=null) {
+			String pageAt = request.getParameter("page");
+			System.out.println("현재 페이지: "+pageAt);
+			cri.setPage(Integer.parseInt(pageAt));
+		}
+		if(request.getParameter("pageSize")!=null) {
+			String pageSize = request.getParameter("pageSize");
+			System.out.println("현재 페이지사이즈: "+pageSize);
+			cri.setPageSize(Integer.parseInt(pageSize));
+		}
+		if(request.getParameter("boardSearch")!=null) {
+			String boardSearch = request.getParameter("boardSearch");
+			System.out.println("현재 boardSearch: "+boardSearch);
+
+		}
+		PageMaker pm = new PageMaker();
+		pm.setCri(cri);
+		pm.setTotalCount(service.pageCount()); //calcDate()실행
+		
+		model.addAttribute("pm", pm);
+		model.addAttribute("cri", cri);
+		return "board/boardMain";
+	}
 	@GetMapping("boardInput")
 	public String boardInputPage() {
 		return "board/boardInput";
@@ -55,7 +116,44 @@ public class BoardController {
 	@PostMapping("boardInsert")
 	public String boardInsert(Board board) {
 		service.insert(board);
-		
-		return "listPage";//여기부터 boardList로 가는법
+		return "redirect:listPage";
+	}
+	@GetMapping("boardContent")
+	public ModelAndView boardContent(Long bnum, HttpServletRequest request) {
+		Board board = service.getBoardContent(bnum);
+		ArrayList<Reply> reply = replyservice.getReply(bnum);
+		ModelAndView mv = new ModelAndView("board/boardMainContent","board",board);
+		if(request.getParameter("page")!=null) {
+			String pageAt = request.getParameter("page");
+			System.out.println("현재 페이지: "+pageAt);
+			mv.addObject("page", pageAt);
+		}
+		if(request.getParameter("pageSize")!=null) {
+			String pageSize = request.getParameter("pageSize");
+			System.out.println("현재 페이지사이즈: "+pageSize);
+			mv.addObject("pageSize", pageSize);
+		}
+		mv.addObject("bnum", bnum);
+		mv.addObject("reply", reply);
+		mv.addObject("totalReply", reply.size());
+		return mv;
+	}
+	@GetMapping("boardDelete")
+	public String boardDelete(Long bnum) {
+		service.delete(bnum);
+		return "redirect:listPage";
+	}
+	@GetMapping("boardUpdate")
+	public ModelAndView boardUpdatePage(Long bnum) {
+		Board board = service.getBoardContent(bnum);
+		ModelAndView mv = new ModelAndView("board/updatePage", "board", board);
+		return mv;
+	}
+	@PostMapping("boardUpdate")
+	public ModelAndView commitBoardUpdate(Board board) {
+		System.out.println("commit~안 board의 bnum: "+ board.getBnum());
+		service.update(board);
+		ModelAndView mv = new ModelAndView("redirect:listPage");
+		return mv;
 	}
 }

6ea9ad46615716f213b2fbaab33201a21589f176
dnjswjaghd <74960228+dnjswjaghd@users.noreply.github.com>
2022-03-29 14:40:29 +0900

    Add files via upload

--- Togather Project/java/team1/togather/domain/Board.java
+++ Togather Project/java/team1/togather/domain/Board.java
@@ -1,12 +1,16 @@
 package team1.togather.domain;
 
 import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatTypes;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
+
 @AllArgsConstructor
 @NoArgsConstructor
 public class Board {
@@ -19,5 +23,68 @@ public class Board {
 	String bfile;
 	Long blike;
 	Long bview;
-	Date rdate; 
+	@JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy-MM-dd (E) HH:mm")
+	Date rdate;
+	public Long getBnum() {
+		return bnum;
+	}
+	public void setBnum(Long bnum) {
+		this.bnum = bnum;
+	}
+	public String getBcategory() {
+		return bcategory;
+	}
+	public void setBcategory(String bcategory) {
+		this.bcategory = bcategory;
+	}
+	public String getBtitle() {
+		return btitle;
+	}
+	public void setBtitle(String btitle) {
+		this.btitle = btitle;
+	}
+	public Long getMnum() {
+		return mnum;
+	}
+	public void setMnum(Long mnum) {
+		this.mnum = mnum;
+	}
+	public String getMname() {
+		return mname;
+	}
+	public void setMname(String mname) {
+		this.mname = mname;
+	}
+	public String getBcontent() {
+		return bcontent;
+	}
+	public void setBcontent(String bcontent) {
+		this.bcontent = bcontent;
+	}
+	public String getBfile() {
+		return bfile;
+	}
+	public void setBfile(String bfile) {
+		this.bfile = bfile;
+	}
+	public Long getBlike() {
+		return blike;
+	}
+	public void setBlike(Long blike) {
+		this.blike = blike;
+	}
+	public Long getBview() {
+		return bview;
+	}
+	public void setBview(Long bview) {
+		this.bview = bview;
+	}
+	public Date getRdate() {
+		return rdate;
+	}
+	public void setRdate(Date rdate) {
+		this.rdate = rdate;
+	} 
+	
+	
 }

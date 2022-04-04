78f4a22f43c414c9e630e93c1be203169ad44f60
dnjswjaghd <74960228+dnjswjaghd@users.noreply.github.com>
2022-04-04 09:28:33 +0900

    Update GatheringController.java

--- Togather Project/java/team1/togather/controller/GatheringController.java
+++ Togather Project/java/team1/togather/controller/GatheringController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.ModelAndView;  
 
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j;
